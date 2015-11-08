b8f6e15f4135df89214eba51c98158de149dc05c
Jack Sneed <sneed.jack@gmail.com>
2015-11-07 20:54:25 -0800

    Working on proper test for content-type.

---
+++ test/write-head-test.js
@@ -0,0 +1,10 @@
+var mocha = require('mocha');
+var chai = require('chai');
+var expect = require('chai').expect;
+var resWriteHead = require(__dirname + '/lib/write-head.js');
+
+describe('the resWriteHead function', function(){
+  it('should return resWriteHead("MIME type"/"internet media type")', function() {
+    expect(resWriteHead(200, 'text/html')).to.br.true;
+  });
+});
