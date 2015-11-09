aed29f9fdc83ef264afb323529fe8a121e445e3e
David Vicklund <david@vicklund.com>
2015-11-08 22:03:19 -0800

    Update test.js

---
+++ test/test.js
@@ -0,0 +1,30 @@
+var chai = require('chai');
+var logger = require('/lib/logger');
+
+describe('logging module', function() {
+  
+  
+  it('should receive requests', function() {
+
+  })
+
+  it('should receive responses', function() {
+
+  })
+
+  it('should process requests', function() {
+
+  })
+
+  it('should process responses', function() {
+
+  })
+
+  it('should write request log to file', function() {
+
+  })
+
+  it('should write response log to file', function() {
+
+  })
+})
