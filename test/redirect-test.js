8e58be3c4f6082e05bbfc3b8457d940046c64584
Ryan Heathers <ryan@ryanheathers.com>
2015-11-08 21:19:21 -0800

    add tests for redirections

--- test/redirect-test.js
+++ test/redirect-test.js
@@ -11,16 +11,38 @@ describe('router functionality', function() {
     this.res = {
       test: 'sometest'
     };
-    this.handler = function(){};
+    this.handler = function(req, res){
+      console.log('Request handled');
+    };
+    this.redirect = 'newurl';
+    this.redirectHandler = function(req, res) {
+      console.log('Redirect handled');
+    };
   });
 
-  it('should create a GET route', function() {
-    debugger;
+  it('should create a standard GET route', function() {
     this.req.method = 'GET';
-    testRoute = this.router.routes[this.req.url];
+    var testRoute = this.router.routes[this.req.url];
     // set the route object
     this.router.setRoute(this.req.method, this.req.url, this.handler);
 
     expect(testRoute[handler]).to.eql(this.handler);
+  }.bind(this));
+
+  it('should create a redirection', function() {
+    this.req.method = 'GET';
+    var testRoute = this.router.routes[this.req.url];
+    // set the route object
+    this.router.setRoute(this.req.method, this.req.url, this.handler, this.redirect);
+
+    expect(testRoute[redirect]).to.eql(true);
+    expect(testRoute[redirectURL]).to.eql('newurl');
+  }.bind(this));
+
+  it('should return a redirection', function() {
+    this.req.method = 'GET';
+    var testRoute = this.router.routes[this.req.url];
+
+    expect(this.router.getRoute(this.req, this.res)).to.eql('Redirect handled');
   }.bind(this));
 });
