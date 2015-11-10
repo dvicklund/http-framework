e4d3517f69f4aed70392ff0c047229e1c9aa1dee
Ryan Heathers <ryan@ryanheathers.com>
2015-11-09 16:33:24 -0800

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-var http = require('http');
 var Router = require(__dirname + '/lib/router');
-// var logger = require(__dirname + '/lib/logger');
-var router = new Router();
 
-http.createServer(function(req, res) {
-  router.route(req, res);
-}).listen(3000, function() {
-  console.log('server up');
-});
+module.exports = exports = new Router();
